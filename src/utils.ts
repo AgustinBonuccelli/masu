732ab6e9f42afebfea00071e00ba36d1335371d0
Diego Bonuccelli <agusnidea27@gmail.com>
2023-12-01 11:16:00 -0300

    feat: add axios function

---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosResponse } from 'axios'
+import { QueryParams } from './types'
+
+/**
+ * 
+ * @param url 
+ * @param params 
+ * @returns { Promise<AxiosResponse> }
+ */
+export const fetch = async (url: string, params?: QueryParams): Promise<AxiosResponse> => {
+  const options = {
+    method: 'GET',
+    url,
+    params,
+    headers: {}
+  }
+  return await axios.request(options)
+}
